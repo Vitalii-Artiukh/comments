07e346ab0d681d6878647433b2be5e73701805f0
Vitalii Artiukh <radial_car@yahoo.com>
2025-11-05 22:25:13 +0200

    typeScript-updated-again

--- src/app/lib/types/types.ts
+++ src/app/lib/types/types.ts
@@ -7,51 +7,46 @@ export interface PostTypes {
   userName?: string;
 }
 
-export interface ApiError {
-  message: string;
-  status?: number;
-}
-
 export interface NavTypes {
-  nav?: {
-    home?: string;
-    about?: string;
+  nav: {
+    home: string;
+    about: string;
   };
 }
 
 export interface ErrorTypes {
-  error?: {
-    unk?: string;
-    again?: string;
+  error: {
+    unk: string;
+    again: string;
   };
 }
 
 export interface NotFoundTypes {
-  notFound?: {
-    title?: string;
-    description?: string;
-    notPost?: string;
+  notFound: {
+    title: string;
+    description: string;
+    notPost: string;
   };
 }
 
 export interface CommonTypes {
-  common?: {
-    posts?: string;
-    readMy?: string;
-    user?: string;
-    id?: string;
-    clickButton?: string;
-    return?: string;
-    began?: string;
+  common: {
+    posts: string;
+    readMy: string;
+    user: string;
+    id: string;
+    clickButton: string;
+    return: string;
+    began: string;
   };
 }
 
 export interface AboutTypes {
-  about?: {
-    name?: string;
-    titleForName?: string;
-    titleForEmail?: string;
-    projectDescription?: string;
+  about: {
+    name: string;
+    titleForName: string;
+    titleForEmail: string;
+    projectDescription: string;
   };
 }
 
